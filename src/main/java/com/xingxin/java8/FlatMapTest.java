7dc9415441c74f456309630267eb6f2972c899b1
liuxinghui <lxh177005@163.com>
2021-07-17 13:09:17 +0800

    java 8 ctrl+alt+L

--- src/main/java/com/xingxin/java8/FlatMapTest.java
+++ src/main/java/com/xingxin/java8/FlatMapTest.java
@@ -1,7 +1,5 @@
 package com.xingxin.java8;
 
-import org.noggit.JSONUtil;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -9,6 +7,7 @@ import java.util.stream.Collectors;
 
 /**
  * map 处理之后，list结构还是原来的，flatMap处理之后，list结果改为一级了
+ *
  * @author liuxh
  * @date 2021/7/15
  */
@@ -23,7 +22,7 @@ public class FlatMapTest {
         // map处理过后，eggs结构还是 List<List<String>>
         eggs.stream()
                 .map(egg -> egg.stream().map(e -> e.replace("鸡", "煎")))
-                .forEach(egg -> egg.forEach(System.out::print) );
+                .forEach(egg -> egg.forEach(System.out::print));
 
         // flatMap处理过后，eggs结构变为：List<String>
         eggs.stream()
@@ -35,6 +34,5 @@ public class FlatMapTest {
         List<String[]> collect = list.stream().map(s -> s.split("")).collect(Collectors.toList());
         List<String> result = collect.stream().flatMap(Arrays::stream).distinct().collect(Collectors.toList());
         System.out.println("result = " + result);
-
     }
 }
