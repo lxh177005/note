88b5432cc3cb7409101ab902d93f1437e05ca6bb
liuxinghui <lxh177005@163.com>
2021-07-22 14:44:29 +0800

    hui

--- src/main/java/com/xingxin/test/HuiTest.java
+++ src/main/java/com/xingxin/test/HuiTest.java
@@ -11,7 +11,7 @@ public class HuiTest {
         String param = "0AF9C0";
         // 16进制->10进制->转2进制
         String left = Integer.toBinaryString(Integer.parseInt(param.substring(0, 2), 16));
-        String right = Integer.toBinaryString(Integer.parseInt(param.substring(2, 6), 16));
+        String right = "0." + Integer.toBinaryString(Integer.parseInt(param.substring(2, 6), 16));
         System.out.println("left = " + left);
         System.out.println("right = " + right);
         char[] chars = left.toCharArray();
@@ -25,8 +25,12 @@ public class HuiTest {
         }
         System.out.println("newLeft = " + String.valueOf(newChar));
         int n = Integer.parseInt(String.valueOf(newChar[0]));
-        int e = Integer.parseInt(String.valueOf(newChar[1]));
-        System.out.println("n=" + n + " ,e=" + e);
+        int m = Integer.parseInt(String.valueOf(newChar[1]));
+        int e = Integer.parseInt(String.valueOf(newChar).substring(2, 8),2);
+        if (m == 1) {
+            e *= (-1);
+        }
+        System.out.println("n=" + n + " ,m=" + m + " ,e=" + e  + " ,right=" + Double.parseDouble(right));
         double result = Math.pow(-1, n) * Math.pow(2, e) * Double.parseDouble(right);
         System.out.println("result = " + result);
     }
