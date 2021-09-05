7165e791f2db055a75152de61554ae1824b43d94
liuxinghui <lxh177005@163.com>
2021-09-05 21:15:04 +0800

    add enum singleton demo

--- src/main/java/com/xingxin/enums/SingletonEnum.java
+++ src/main/java/com/xingxin/enums/SingletonEnum.java
@@ -3,6 +3,12 @@ package com.xingxin.enums;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Arrays;
+
 /**
  *
  * 枚举单例，不仅能避免多线程的同步问题，还能防止java反射和反序列化创建新的对象
@@ -15,9 +21,37 @@ public enum SingletonEnum {
     /**
      * 枚举单例
      */
-    INSTANCE;
+    INSTANCE, ENUM_TEST;
 
     public void hello() {
         System.out.println("hello");
+    }
+
+    public static void main(String[] args) {
+        /*
+         * javap -p E:\learn\note\target\classes\com\xingxin\enums\SignletonEnum.class
+         * 反编译 枚举类
+            Compiled from "SignletonEnum.java"
+            public final class com.xingxin.enums.SignletonEnum extends java.lang.Enum<com.xingxin.enums.SignletonEnum> {
+            public static final com.xingxin.enums.SignletonEnum INSTANCE;
+            private static final com.xingxin.enums.SignletonEnum[] $VALUES;
+            public static com.xingxin.enums.SignletonEnum[] values();
+            public static com.xingxin.enums.SignletonEnum valueOf(java.lang.String);
+            private com.xingxin.enums.SignletonEnum();
+            public void hello();
+            public static void main(java.lang.String[]);
+            static {};
+           }
+         */
+        SingletonEnum instance = SingletonEnum.INSTANCE;
+        System.out.println("instance = " + instance);
+        SingletonEnum.INSTANCE.hello();
+        SingletonEnum[] values = SingletonEnum.values();
+        System.out.println("values = " + Arrays.toString(values));
+        SingletonEnum instance1 = SingletonEnum.valueOf("INSTANCE");
+        System.out.println("instance1 = " + instance1);
+        System.out.println(instance == instance1);
+
+
     }
 }
--- src/main/java/com/xingxin/test/LinkedHashMapTest.java
+++ src/main/java/com/xingxin/test/LinkedHashMapTest.java
@@ -1,7 +1,5 @@
 package com.xingxin.test;
 
-import org.apache.solr.common.util.Hash;
-
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -13,18 +11,18 @@ import java.util.Map;
 public class LinkedHashMapTest {
     public static void main(String[] args) {
         Map<String, String> map = new LinkedHashMap<>();
-        map.put("1","aaa");
-        map.put("2","bbb");
-        map.put("3","ccc");
-        map.forEach( (k, v) -> System.out.print( k + ":" + v));
+        map.put("1", "aaa");
+        map.put("2", "bbb");
+        map.put("3", "ccc");
+        map.forEach((k, v) -> System.out.print(k + ":" + v));
         map.get("2");
-        map.forEach( (k, v) -> System.out.print( k + ":" + v));
+        map.forEach((k, v) -> System.out.print(k + ":" + v));
         map.get("3");
-        map.forEach( (k, v) -> System.out.print( k + ":" + v));
+        map.forEach((k, v) -> System.out.print(k + ":" + v));
 
-        HashMap<String, String> hashMap = new HashMap<>(2);
+        HashMap<String, String> hashMap = new HashMap<>();
         hashMap.put("aaa", null);
         hashMap.put("bbb", null);
-        hashMap.forEach( (k, v) -> System.out.print( k + ":" + v));
+        hashMap.forEach((k, v) -> System.out.print(k + ":" + v));
     }
 }
